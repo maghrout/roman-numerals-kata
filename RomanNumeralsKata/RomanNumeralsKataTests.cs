d594e651ad02735596f4e128c35aa182fa9180b3
Moujib Aghrout <moujib.aghrout@codecomputerlove.com>
2019-01-05 14:32:00 +0000

    refactor: create RomanNumeral class, create list of roman numerals, replace if else if with foreach

--- RomanNumeralsKata/RomanNumeralsKataTests.cs
+++ RomanNumeralsKata/RomanNumeralsKataTests.cs
@@ -3,6 +3,7 @@ using Xunit;
 namespace RomanNumeralsKata
 {
     using System;
+    using System.Collections.Generic;
 
     public class RomanNumeralsKataTests
     {
@@ -20,6 +21,12 @@ namespace RomanNumeralsKata
 
     public class NumberToRomanNumeralsMapper
     {
+        private static readonly List<RomanNumeral> RomanNumerals = new List<RomanNumeral>
+        {
+            new RomanNumeral{Symbol = "I", Value = 1},
+            new RomanNumeral{Symbol = "II",Value = 2},
+            new RomanNumeral{Symbol = "III",Value = 3},
+        };
         private readonly string _romanNumeralString;
 
         private NumberToRomanNumeralsMapper(string romanNumeralString)
@@ -32,24 +39,13 @@ namespace RomanNumeralsKata
             var romanNumeralString = "";
             while (input != 0)
             {
-                if (input == 3)
+                foreach (var romanNumeral in RomanNumerals)
                 {
-                    romanNumeralString += "III";
-                    input -= input;
-                }
-                else if (input == 2)
-                {
-                    romanNumeralString += "II";
-                    input -= input;
-                }
-                else if (input == 1)
-                {
-                    romanNumeralString += "I";
-                    input -= input;
-                }
-                else
-                {
-                    throw new ArgumentException();
+                    if (input == romanNumeral.Value)
+                    {
+                        romanNumeralString += romanNumeral.Symbol;
+                        input -= romanNumeral.Value;
+                    }
                 }
             }
 
@@ -60,5 +56,11 @@ namespace RomanNumeralsKata
         {
             return _romanNumeralString;
         }
+    }
+
+    public class RomanNumeral
+    {
+        public int Value { get; set; }
+        public string Symbol { get; set; }
     }
 }
